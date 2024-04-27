c83493e622d555426c57a366170a827f90d29a0c
mma <111668501+mmalkindi@users.noreply.github.com>
2024-04-27 10:59:10 +0700

    Add apps to settings

--- pacilflix/settings.py
+++ pacilflix/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'content',
+    'contributor',
+    'download',
+    'favorite',
+    'review',
+    'subscription',
 ]
 
 MIDDLEWARE = [
