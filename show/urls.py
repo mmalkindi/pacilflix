61480ba7ead6c1f5631d0f684cfbc7c6db3b3f5c
Fec16 <jasonkentwinata@gmail.com>
2024-05-02 15:52:37 +0700

    Add feat/tayangan

---
+++ show/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from show.views import show_main, show_tayangan, show_series, show_film, show_episode
+
+urlpatterns = [
+    path('', show_main, name='show_main'),
+    path('tayangan', show_tayangan, name='tayangan'),
+    path('series', show_series, name='series'),
+    path('film', show_film, name='film'),
+    path('episode', show_episode, name='episode'),
+]
