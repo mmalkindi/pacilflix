548b6be8316f82b6b87bb1e5fdca65a7320c0d95
Michelle Elizabeth Amanda Hutasoit <michelleelizabethamandahutasoit@Michelles-MacBook-Pro.local>
2024-05-02 19:09:08 +0700

    Add modal for download and favorite button

--- show/urls.py
+++ show/urls.py
@@ -3,8 +3,8 @@ from show.views import show_main, show_tayangan, show_series, show_film, show_ep
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('tayangan', show_tayangan, name='tayangan'),
-    path('series', show_series, name='series'),
-    path('film', show_film, name='film'),
-    path('episode', show_episode, name='episode'),
+    path('tayangan/', show_tayangan, name='tayangan'),
+    path('series/', show_series, name='series'),
+    path('film/', show_film, name='film'),
+    path('episode/', show_episode, name='episode'),
 ]
